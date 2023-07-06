2befb855b200e325915fbd579e0f33bbc7c4caa1
Ebenezer <akhonyaebenezer@gmail.com>
2023-07-06 15:28:02 +0300

    [Chore]: Add task one

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+Type annotated function
+for adding two numbers
+"""
+
+
+def add(a: float: b: float) -> float:
+    """
+    Function to add two numbers together
+    Args:
+        a (float): first number to add
+        b (float): second number to add
+    Returns:
+        returns a float
+   """
+    if isinstance(a, float) and isinstance(b, float):
+        return a + b
+    raise TypeError('Enter float values')
