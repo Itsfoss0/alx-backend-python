6a83492214754605eaca6fa7fc41fde6a3cb774e
Ebenezer <akhonyaebenezer@gmail.com>
2023-07-07 18:00:31 +0300

    [Chore]: Add task six

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+type annotated funtion to sum list items
+if they are floats
+"""
+
+from functools import reduce
+
+
+def sum_list(input_list: list) -> float:
+    """
+    function to sum list items
+    Args:
+        input_list (list): a list of floats
+    Returns
+        returns a float
+    """
+    if not isinstance(input_list, list):
+        raise TypeError("expected a list as input")
+    return float(reduce(lambda x, y: x + y, input_list))
+
+
+if __name__ == "__main__":
+    print(sum_list([1,2,3,4]))
