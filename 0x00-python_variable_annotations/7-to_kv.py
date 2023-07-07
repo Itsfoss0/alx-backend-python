a52a7fc0747471df9eb9ef8e39ad318b18a0711c
Ebenezer <akhonyaebenezer@gmail.com>
2023-07-07 20:41:20 +0300

    [Chore]: Add task eight

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+Module with a type annotated function
+that returns a tuple
+"""
+
+
+from typing import Tuple, Union
+
+v_type = Union[float, int]
+return_type = Union[str, float]
+
+def to_kv(k: str, v: v_type) -> return_type:
+    """
+    conver k, v to a tuple
+    Args:
+        k (str): any valid string value
+        v (int/float): An int or float value
+    """
+    if not isinstance(k, str):
+        raise TypeError('expected a string')
+    v_squared: float = v * v
+    return tuple([k, v_squared])
