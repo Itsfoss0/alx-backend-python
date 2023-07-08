80238fecd588f2c9b79ae24b2ba25635c2ce15d7
Ebenezer <akhonyaebenezer@gmail.com>
2023-07-08 12:42:56 +0300

    [Chore]: Add Advanced task one

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from typing import Sequence, Any, Union
+
+lst_type = Sequence[Any]
+return_type = Union[Any, None]
+
+
+def safe_first_element(lst: lst_type) -> return_type:
+    """
+    Get the first element in a sequence safely
+    If the there's no elements, return None
+    Args:
+        lst (list): A list of elements of any type
+    Returns:
+        if  element != None; Return first element
+        else return None
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
