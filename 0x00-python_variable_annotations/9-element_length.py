c874ae30afc2c31b77a1a883d63120d6a1377be8
Ebenezer <akhonyaebenezer@gmail.com>
2023-07-08 12:07:18 +0300

    [Chore]: Add task ten

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -12,4 +12,11 @@ return_type = List[Tuple[Sequence, int]]
 
 
 def element_length(lst: lst_type) -> return_type:
+    """
+    Function to get the length of elements in an iterable
+    Args:
+        lst (list): A list of elements
+    Returns:
+        Returns a list
+    """
     return [(i, len(i)) for i in lst]
