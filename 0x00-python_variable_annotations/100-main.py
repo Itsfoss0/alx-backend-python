1a8db466757622262592998eba5bb015304dca09
Ebenezer <akhonyaebenezer@gmail.com>
2023-07-10 09:54:50 +0300

    [Chore]: Test Annoatatation basics

---
+++ 0x00-python_variable_annotations/100-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+safe_first_element =  __import__('100-safe_first_element').safe_first_element
+
+print(safe_first_element.__annotations__)
+print(safe_first_element({}, "sherlock", "holmes"))
