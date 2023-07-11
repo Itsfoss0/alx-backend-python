f9e02fd8f7af2193ecaa4daf0b5aedf8b50159e2
Ebenezer <akhonyaebenezer@gmail.com>
2023-07-11 18:50:30 +0530

    [Chore]: Add task three

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env  python3
+
+
+"""
+module with a fuction to compute the
+average excetion time for running a subroutine
+"""
+
+from asyncio import run
+from time import time
+wait_n = __import__('1-concurent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int):
+    """
+    Measure the excetution time of a subroutine
+    Args:
+        n (int): number of times to run the subroutine
+        max_delay (int): maximum amout of time to delay
+    Returns:
+        returns a float
+    """
+    if isinstance(n, int) and isinstance(max_delay, int):
+        c_time = time()
+        run(wait_n(n, max_delay))
+        return (time() - c_time) / n
+    raise TypeError('expected integer values to be passed!')
