9f27fa6779eccdd7164531d85cc191d1d2b18389
Ebenezer <akhonyaebenezer@gmail.com>
2023-07-11 17:00:58 +0530

    [Chore]: Add task one

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+basics of concurent programming in python
+using the asyncio library
+"""
+
+from random import random
+from asyncio import sleep
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    This coroutine waits for a few seconds
+    Args:
+        max_delay (int)
+    """
+    wait_time = random() * max_delay
+    await sleep(wait_time)
+    return wait_time
