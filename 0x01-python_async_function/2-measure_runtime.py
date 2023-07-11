d57c5032463799d40e0d8d36873758c19459ae8a
Ebenezer <akhonyaebenezer@gmail.com>
2023-07-11 19:13:29 +0530

    [Chore ]: Fix task three

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -8,7 +8,7 @@ average excetion time for running a subroutine
 
 from asyncio import run
 from time import time
-wait_n = __import__('1-concurent_coroutines').wait_n
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int):
