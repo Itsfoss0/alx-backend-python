c652f8fded00627c1a3a510b75bd4dacf725f36b
Itsfoss0 <akhonyaebenezer@gmail.com>
2023-07-15 00:20:04 +0300

    [Chore]: Add task one

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+
+"""
+Module with an async generator
+Generates random numbers between
+one and ten
+"""
+
+from asyncio import sleep
+from random import random
+from typing import Generator
+
+
+async def async_generator() -> Generator:
+    """
+    Generate numbers between one and ten
+    Args:
+        None
+    Returns:
+        Generator
+    """
+    for _ in range(0, 10):
+        yield random() * 10
+        await sleep(1)
