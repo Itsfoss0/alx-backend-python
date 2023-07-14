25e62eef75df5cf403d62bbd924fe93d99b88549
Itsfoss0 <akhonyaebenezer@gmail.com>
2023-07-15 00:23:14 +0300

    [Chore]: Add task one

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -20,5 +20,6 @@ async def async_generator() -> Generator[float, None, None]:
         Generator
     """
     for _ in range(0, 10):
-        yield random() * 10
         await sleep(1)
+        yield random() * 10
+
