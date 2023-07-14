55c9003810c65f7c7bf1318b73d4f43a58638110
Itsfoss0 <akhonyaebenezer@gmail.com>
2023-07-15 00:27:54 +0300

    [Chore]: Add task two

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+"""
+Module to generate random numbers as well
+"""
+
+
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+return_type = List[float]
+
+
+async def async_comprehension() -> return_type:
+    """
+    Returns some 10 random numbers based on the
+    other function defined in the previous module
+    """
+    return [num async for num in async_generator()]
